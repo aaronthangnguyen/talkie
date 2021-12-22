8a805d7aabd65ea459680f73ddcbff8a17e2dce5
Aaron Nguyen <me@aaronnguyen.dev>
2021-12-21 19:22:10 -0500

    Create first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# TALKIE (TRY SOCKET.IO)
+
+![Imgur](https://imgur.com/a/lx5YXim.png)
+
+## Technologies
+
+- [Express.js](https://expressjs.com/)
+- [Socket.IO](https://socket.io/get-started/chat)
+- [Spectre.CSS](https://picturepan2.github.io/spectre/)
+- [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Installation
+
+```bash
+git clone https://github.com/aaronthangnguyen/talkie-try-socketio
+cd talkie-try-socketio
+```
+
+```bash
+# pnpm required, check out https://pnpm.io/installation
+pnpm i
+pnpm dev
+```
+
+Go to http://localhost:3001 to try it out 🚀
---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import http from "http";
+import { __dirname } from "./utils";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use("/public", express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  // console.log("A person is connected");
+
+  // socket.on("disconnect", () => {
+  //   console.log("A person is disconnected");
+  // });
+
+  socket.on("message", (messageContent) => {
+    io.emit("message", messageContent);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is running on http://localhost:3001");
+});
